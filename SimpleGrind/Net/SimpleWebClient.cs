6c2ac44fe3e0a1f4f890a7ba47f1ae4b7bde750e
Tobias Hasslebrant <tobias.hasslebrant@gmail.com>
2015-08-19 11:18:07 +0200

    refactoring

--- SimpleGrind/Net/SimpleWebClient.cs
+++ SimpleGrind/Net/SimpleWebClient.cs
@@ -43,46 +43,14 @@ namespace SimpleGrind.Net
 				_httpClient.DefaultRequestHeaders.Add(header.Key,header.Value);
 	    }
 
-		public async Task<HttpResponseMessage> GetAsync(string url)
-		{
-			return await _httpClient.GetAsync(url);
-		}
-		
-		public HttpResponseMessage Get(string url)
-		{
-			return GetAsync(url).Result;
-		}
+		public async Task<HttpResponseMessage> GetAsync(string url) => await _httpClient.GetAsync(url);
+        public async Task<HttpResponseMessage> PostJsonAsync(string url, string json) => await _httpClient.PostAsync(url, new StringContent(json, Encoding.UTF8, "application/json"));
+		public async Task<HttpResponseMessage> PutJsonAsync(string url, string json) => await _httpClient.PutAsync(url, new StringContent(json, Encoding.UTF8, "application/json"));
+		public async Task<HttpResponseMessage> DeleteAsync(string url) => await _httpClient.DeleteAsync(url);
 
-		public async Task<HttpResponseMessage> PostJsonAsync(string url, string json)
-		{
-			var content = new StringContent(json, Encoding.UTF8, "application/json");
-			return await _httpClient.PostAsync(url, content);
-		}
-		
-		public HttpResponseMessage PostJson(string url, string json)
-		{
-			return PostJsonAsync(url, json).Result;
-		}
-
-		public async Task<HttpResponseMessage> PutJsonAsync(string url, string json)
-		{
-			var content = new StringContent(json, Encoding.UTF8, "application/json");
-			return await _httpClient.PutAsync(url, content);
-		}
-
-		public HttpResponseMessage PutJson(string url, string json)
-		{
-			return PutJsonAsync(url, json).Result;
-		}
-
-		public async Task<HttpResponseMessage> DeleteAsync(string url)
-		{
-			return await _httpClient.DeleteAsync(url);
-		}
-
-		public HttpResponseMessage Delete(string url)
-		{
-			return DeleteAsync(url).Result;
-		}
+        public HttpResponseMessage Get(string url) => GetAsync(url).Result;
+        public HttpResponseMessage PostJson(string url, string json) => PostJsonAsync(url, json).Result;
+        public HttpResponseMessage PutJson(string url, string json) => PutJsonAsync(url, json).Result;
+        public HttpResponseMessage Delete(string url) => DeleteAsync(url).Result;
 	}
 }
