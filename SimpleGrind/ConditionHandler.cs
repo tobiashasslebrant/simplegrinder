f5352fe591cc53550ec03fc840ed362ed7a27a26
Tobias Hasslebrant <tobias.hasslebrant@gmail.com>
2019-02-12 11:51:42 +0100

    removed wrong code

--- SimpleGrind/ConditionHandler.cs
+++ SimpleGrind/ConditionHandler.cs
@@ -23,32 +23,22 @@ namespace SimpleGrind
             var cond = Regex.Match(_runnerParameters.ErrorCondition,"^(.+)([<>!=%#])(.+)$");
             var field = cond.Groups[1].Value;
             var condition = cond.Groups[2].Value;
-            var val = cond.Groups[3].Value;
+            var val = long.Parse( cond.Groups[3].Value);
             var errors = new List<string>();
              
             switch (field)
             {
                 case "totaltime":
-                    if(HasError(aggregatedResult.TotalTime, 0))
+                    if(HasError(aggregatedResult.TotalTime))
                         errors.Add($"condition: {_runnerParameters.ErrorCondition}. Totaltime: {aggregatedResult.TotalTime}");
                     break;
                 case "totalavg":
-                    if(HasError(aggregatedResult.AverageTime, 0))
+                    if(HasError(aggregatedResult.AverageTime))
                         errors.Add($"condition: {_runnerParameters.ErrorCondition}. Averagetime: {aggregatedResult.AverageTime}");
                     break;
                 case "totalerrors":
                     if(HasError(errorCount, aggregatedResult.TotalCalls))
                         errors.Add($"condition: {_runnerParameters.ErrorCondition}. Totalerrors: {errorCount}, Totalcalls: {aggregatedResult.TotalCalls}");
-                    break;
-                case "time":
-                    foreach (var runResult in aggregatedResult.RunResults)
-                        if(HasError(runResult.TotalTime, aggregatedResult.TotalTime))
-                            errors.Add($"condition: {_runnerParameters.ErrorCondition}. Time: {runResult.TotalTime}, TotTime: {aggregatedResult.TotalTime}");
-                    break;
-                case "avg":
-                    foreach (var runResult in aggregatedResult.RunResults)
-                        if(HasError(runResult.AverageTime, runResult.NumberOfCalls))
-                            errors.Add($"condition: {_runnerParameters.ErrorCondition}. AverageTime: {runResult.AverageTime}, TotAverageTime: {aggregatedResult.AverageTime}");
                     break;
                 case "ok":
                     foreach (var runResult in aggregatedResult.RunResults)
@@ -65,27 +55,29 @@ namespace SimpleGrind
                         if(HasError(runResult.TimedOut, runResult.NumberOfCalls))
                             errors.Add($"condition: {_runnerParameters.ErrorCondition}. Timedout: {runResult.TimedOut}, NumberOfCalls: {runResult.NumberOfCalls}");
                     break;
+                case "time":
+                    foreach (var runResult in aggregatedResult.RunResults)
+                        if(HasError(runResult.TotalTime, aggregatedResult.TotalTime))
+                            errors.Add($"condition: {_runnerParameters.ErrorCondition}. Time: {runResult.TotalTime}, TotTime: {aggregatedResult.TotalTime}");
+                    break;
+                case "avg":
+                    foreach (var runResult in aggregatedResult.RunResults)
+                        if(HasError(runResult.AverageTime, runResult.NumberOfCalls))
+                            errors.Add($"condition: {_runnerParameters.ErrorCondition}. AverageTime: {runResult.AverageTime}, TotAverageTime: {aggregatedResult.AverageTime}");
+                    break;
             }
             return (errors.Any(), errors.ToArray());
 
-            bool HasError<T>(T fieldValue, long percentageCompare)
+            bool HasError(long fieldValue, long percentageCompare = 0)
             {
                 switch (condition)
                 {
-                    case "=": return (Equals(fieldValue, (T) Convert.ChangeType(val, typeof(T))));
-                    case "!": return !(Equals(fieldValue, (T) Convert.ChangeType(val, typeof(T))));
-                    case ">":
-                        return (int) Convert.ChangeType(fieldValue, typeof(int)) >
-                               (int) Convert.ChangeType(val, typeof(int));
-                    case "<":
-                        return (int) Convert.ChangeType(fieldValue, typeof(int)) <
-                               (int) Convert.ChangeType(val, typeof(int));
-                    case "%":
-                        return (int) Convert.ChangeType(fieldValue, typeof(int)) * 100 / percentageCompare >
-                               (int) Convert.ChangeType(val, typeof(int));
-                    case "#":
-                        return (int) Convert.ChangeType(fieldValue, typeof(int)) * 100 / percentageCompare <
-                               (int) Convert.ChangeType(val, typeof(int));
+                    case "=": return fieldValue == val;
+                    case "!": return fieldValue != val;
+                    case ">": return fieldValue > val;
+                    case "<": return fieldValue > val;
+                    case "%": return fieldValue * 100 / percentageCompare > val;
+                    case "#": return fieldValue * 100 / percentageCompare < val;
                     default: return false;
                 }
             }
